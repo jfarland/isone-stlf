f381f8ac53f42e75a40007860ee29a8db25e85be
jfarland <jonfarland@gmail.com>
2015-06-07 12:40:51 -0700

    initial commit of .gitignore and template for semi parametric forecaster code

---
+++ code/02-train-forecaster.R
@@ -0,0 +1,25 @@
+"""
+
+Incredible References:
+(1) Introduction to GLM's in R - http://statmath.wu.ac.at/courses/heather_turner/glmCourse_001.pdf
+(2) Amazing LATTICE Reference -  http://www.isid.ac.in/~deepayan/R-tutorials/labs/04_lattice_lab.pdf
+(3) Reference on Stepwise Regression w/GLMs http://data.princeton.edu/R/glms.html
+
+"""
+
+#include packages
+library("date")
+library("lattice")
+library("forecast")
+library("XLConnect")
+library("gdata")
+library("quantreg")
+library("corrgram")
+library("splines")
+library("plyr")
+
+library("date")
+library("lattice")
+library("forecast")
+library("lfe")
+
