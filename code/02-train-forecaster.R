f10e65e3e36c4dfa22b39780a262b9a090eb88cb
jfarland <jonfarland@gmail.com>
2015-06-17 23:07:42 -0700

    added functionality for lags and leads, improved the SPM models

--- code/02-train-forecaster.R
+++ code/02-train-forecaster.R
@@ -16,7 +16,9 @@ library("gdata")
 library("quantreg")
 library("corrgram")
 library("splines")
-library("plyr")
+library("dplyr")
+library("ggplot2")
+library("lubridate")
 
 library("date")
 library("lattice")
@@ -25,6 +27,7 @@ library("nlme")
 library("mgcv")
 library("lfe")
 library("SemiPar")
+library("lubridate")
 
 """
 import data set
@@ -46,27 +49,70 @@ plot(dat$temp, dat$load)
 create function to prepare modeling data set
 """
 
-#create calendar variables
-
-LFVars <- function()
-
-
 trn <- subset(dat, year<=2011)
 tst <- subset(dat, year>2011)
-
-trn <- lag(trn$load, k=24)
-
 trn0 <- data.frame(trn)
+tst0 <- data.frame(tst)
+
+"""
+User Defined Functions
+"""
+
+#create a date time variable
+toDate <- function(year, month, day) 
+  {
+  ISOdate(year, month, day)
+  }
+
+#create lag and lead variables
+#source <- http://ctszkin.com/2012/03/11/generating-a-laglead-variables/
+shift<-function(x,shift_by){
+  stopifnot(is.numeric(shift_by))
+  stopifnot(is.numeric(x))
+  
+  if (length(shift_by)>1)
+    return(sapply(shift_by,shift, x=x))
+  
+  out<-NULL
+  abs_shift_by=abs(shift_by)
+  if (shift_by > 0 )
+    out<-c(tail(x,-abs_shift_by),rep(NA,abs_shift_by))
+  else if (shift_by < 0 )
+    out<-c(rep(NA,abs_shift_by), head(x,-abs_shift_by))
+  else
+    out<-x
+  out
+}
+
+#date<- ISOdate(trn0$year, trn0$month, trn0$day)
 
 
+#create lag variables of load
+displacements <- seq(24, 168, 24)
+
+#vector of column names
+lagnames <- paste("lag", displacements, sep = "")
+
+cols <- dim(trn0)[2] #number of columns before we add lags
+
+for (i in 1 : length(displacements))
+{
+  disp = displacements[i]
+  trn0[,i+cols] <- unlist(shift(trn0$load, -1*disp))
+  colnames(trn0)[c(i+cols)] = lagnames[i]
+}
 
 """
 train model
 """
-fit1 <- spm(trn$load ~ f(trn$temp) + f(trn$hum) + trn$ws + trn$cc +factor(month)+factor(year)+, group=trn$hour, family = "gaussian",
-              spar.method = "REML", omit.missing=NULL)
+
+fit1 <- lme(load ~ factor(month) + factor(year) + factor(day) + factor(hour) + lag24 +lag48 +lag72 +lag96 +lag120 +lag144 + lag168, data = trn0, random = temp + hum + ws + cc, na.action = na.exclude)
+
+
+fit1 <- spm(trn0$load ~  f(trn0$temp)+f(trn0$hum)+ factor(trn0$month) + trn0$lag24 +trn0$lag48 +trn0$lag72 +trn0$lag96 +trn0$lag120 +trn0$lag144 + trn0$lag168, group=trn$hour, family = "gaussian",
+              spar.method = "REML", omit.missing=TRUE)
 
 summary(fit1)
-plot(fit1$fitted)
-tstDF <- 
-fcst <- predict(fit1, newdata=tst, se=TRUE)
+plot(fit1)
+anova(fit1)
+fcst <- predict(fit1, newdata=tst0, se=TRUE)
